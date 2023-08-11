1da8e8a4a2cbc5721e21a8946cadb622703a9334
Sophie <dunfiena@gmail.com>
2023-08-11 11:37:34 -0300

    Working model August 09, 2023

--- GUI_Kramers-Kronig.py
+++ GUI_Kramers-Kronig.py
@@ -1,13 +1,14 @@
 import math
 import os
 import sys
+import shutil
 
 import matplotlib.pyplot as plt
 import numpy as np
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFileDialog, \
-    QGroupBox, QWidget, QGridLayout
+    QGroupBox, QWidget, QGridLayout, QLineEdit, QMessageBox
 
 import Kramers_Kronig_Calculation as kkc
 
@@ -19,9 +20,16 @@ class MainWindow(QMainWindow):
     def get_filename(self):
         return self._filename
 
+    def set_output_dir(self,x):
+        self._output_dir = x
+
+    def get_output_dir(self):
+        return self._output_dir
+
     def __init__(self):
         # region setup
         super().__init__()
+        self._output_dir = None
         self._filename = None
         self.title = "Kramers-Konig Relation"
         self.setWindowIcon(QIcon("Assets/project_icon_2.png"))
@@ -33,6 +41,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
         app.setStyleSheet('QLabel{font-size: 16pt;}'
                           'QComboBox{font-size: 12pt;}'
+                          'QLineEdit{font-size: 16pt;}'
                           'QPushButton{font-size: 12pt;}'
                           'QSpinBox{font-size: 12pt;}'
                           'QDoubleSpinBox{font-size: 12pt;}'
@@ -46,16 +55,14 @@ class MainWindow(QMainWindow):
         sub.setGeometry(self.left, self.top, self.width, self.height)
         layout = QGridLayout(self)
 
-        file_sel = QLabel('Select File:', self)
-
-        search = QPushButton('Search', self)
+        search = QPushButton('Select File', self)
         search.clicked.connect(self.input_file)
-        search.setFixedSize(300, 50)
+        search.setFixedSize(250, 100)
 
         right_top_group = QGroupBox(self)
 
         run = QPushButton('Run Calculation', self)
-        run.setFixedSize(300, 50)
+        run.setFixedSize(250, 100)
         run.clicked.connect(self.start)
 
         bottom_text = QLabel("Acadia Physics 2023", self)
@@ -75,22 +82,38 @@ class MainWindow(QMainWindow):
         self.firstGraph = QLabel(self)
         self.secondGraph = QLabel(self)
 
+        self.output_button = QPushButton("Output Directory", self)
+        self.output_button.clicked.connect(self.set_output)
+        self.output_button.setFixedSize(250, 100)
+
+        self.file_name = QLineEdit(self)
+        save_label = QLabel("File Name:")
+        save_button = QPushButton("Save")
+        save_button.setFixedSize(250, 50)
+        save_button.clicked.connect(self.save_figure)
+
         space = QLabel()
 
         layout.addWidget(space, 0, 0, 72, 0)  # left side
         layout.addWidget(space, 0, 0, 0, 96)  # top
         layout.addWidget(right_top_group, 1, 20, 2, -1)
 
-        layout.addWidget(run, 3, 60, 2, 20)
+        layout.addWidget(run, 1, 75, 2, 15)
+        layout.addWidget(self.output_button, 1, 55, 2, 15)
+        layout.addWidget(search, 1, 25, 2, 15)
+
         layout.addWidget(bottom_text, 70, 80, 2, 20)
-        layout.addWidget(file_sel, 1, 30, 2, 20)
-        layout.addWidget(search, 1, 60, 2, 20)
         layout.addWidget(logo_img, 1, 0, 2, 20)
         layout.addWidget(logo, 2, 0, 2, 20)
-        layout.addWidget(self.graph1_Title, 6, 1, 2, 20)
-        layout.addWidget(self.graph2_Title, 6, 55, 2, 20)
+
+        layout.addWidget(self.graph1_Title, 6, 1, 2, 30)
+        layout.addWidget(self.graph2_Title, 6, 55, 2, 30)
         layout.addWidget(self.firstGraph, 10, 0, 46, 46)
         layout.addWidget(self.secondGraph, 10, 55, 46, 46)
+
+        layout.addWidget(self.file_name, 67, 12, 4, 40)
+        layout.addWidget(save_label, 68, 1, 2, 20)
+        layout.addWidget(save_button, 66, 55, 6, 6)
 
         sub.setLayout(layout)
         self.show()
@@ -98,44 +121,58 @@ class MainWindow(QMainWindow):
 
     # region Functions
     def start(self):
-        h = 1.05457 * math.pow(10, -34)
-        de = (0.1 * math.pow(10, 13)) * h
-        cshift = 1e-6
+        plt.clf()
+        if window.get_filename():
+            if window.get_output_dir() is None:
+                window.set_output_dir(".")
 
-        f = open('{}'.format(window.get_filename()), 'r')
-        x_axis = []
-        spectra = []
-        x_tick = []
-        i = 0
-        for line in f:
-            i = i + 1
-            line = line.strip().split('\t')
-            x_axis.append(line[0])
-            spectra.append(line[1])
-            if i % 10 == 1:
-                x_tick.append((line[0]))
-        real = kkc.kkr((float(x_axis[1]) - float(x_axis[0])), window.get_filename(), cshift)
-        spectra = np.array(spectra, np.float32)
-        real = np.array(real, dtype=np.float32)
-        x = np.array(x_tick, np.complex_)
-        x_axis = np.array(x_axis, np.complex_)
+            h = 1.05457 * math.pow(10, -34)
+            de = (0.1 * math.pow(10, 13)) * h
+            cshift = 1e-6
 
-        font = {'size': 8}
-        plt.rc('font', **font)
-        plt.plot(x_axis, real[:, 0, 0])
-        plt.plot(x_axis, spectra)
-        plt.xticks(x, x_tick, size='small')
+            f = open('{}'.format(window.get_filename()), 'r')
+            x_axis = []
+            spectra = []
+            x_tick = []
+            i = 0
+            for line in f:
+                i = i + 1
+                line = line.strip().split('\t')
+                x_axis.append(line[0])
+                spectra.append(line[1])
+                if i % 10 == 1:
+                    x_tick.append((line[0]))
+            real = kkc.kkr((float(x_axis[1]) - float(x_axis[0])), window.get_filename(), cshift)
+            spectra = np.array(spectra, np.float32)
+            real = np.array(real, dtype=np.float32)
+            x = np.array(x_tick, np.complex_)
+            x_axis = np.array(x_axis, np.complex_)
+            real = real[:, 0, 0]
+            font = {'size': 8}
+            plt.rc('font', **font)
+            plt.plot(x_axis, real)
+            plt.plot(x_axis, spectra)
+            plt.xticks(x, x_tick, size='small')
 
-        plt.savefig("./output.png", bbox_inches='tight', pad_inches=0.25)
-        pixmap = QPixmap("./output.png")
-        pixmap_resized = pixmap.scaled(525, 400)
+            plt.savefig("tmp_files/tmp_output.png", bbox_inches='tight', pad_inches=0.25)
+            pixmap = QPixmap("tmp_files/tmp_output.png")
+            pixmap_resized = pixmap.scaled(525, 400)
 
-        self.secondGraph.setPixmap(pixmap_resized)
-        self.secondGraph.adjustSize()
+            self.secondGraph.setPixmap(pixmap_resized)
+            self.secondGraph.adjustSize()
+
+            i = 0
+            with open('tmp_files/tmp_txt.txt', 'w') as f:
+                for _ in x_axis:
+                    f.write('{}   {}   {}\n'.format(x_axis[i], spectra[i], real))
+                    i = i + 1
+        else:
+            print("No file selected")
 
     def input_file(self):
+        plt.clf()
         self.firstGraph.clear()
-        tmp = "./tmp.png"
+        tmp = "tmp_files/tmp.png"
         if os.path.isfile(tmp):
             os.remove(tmp)
 
@@ -164,8 +201,8 @@ class MainWindow(QMainWindow):
             plt.rc('font', **font)
             plt.plot(x_axis, spectra)
             plt.xticks(x, x_tick, size='small')
-            plt.savefig("./tmp.png", bbox_inches='tight', pad_inches=0.25)
-            pixmap = QPixmap("./tmp.png")
+            plt.savefig("tmp_files/tmp.png", bbox_inches='tight', pad_inches=0.25)
+            pixmap = QPixmap("tmp_files/tmp.png")
             pixmap_resized = pixmap.scaled(525, 400)
             self.firstGraph.setPixmap(pixmap_resized)
             self.firstGraph.adjustSize()
@@ -175,11 +212,22 @@ class MainWindow(QMainWindow):
     def set_output(self):
         output_path = QFileDialog().getExistingDirectory(self, None, "Select Folder")
         if output_path:
-            window.set_output_path(output_path)
+            window.set_output_dir(output_path)
     # endregion
 
+    def save_figure(self):
+        name = self.file_name.text()
+        tmp_out = "tmp_files/tmp_output.png"
+        if os.path.isfile(tmp_out):
+            shutil.move(tmp_out, '{}/{}.png'.format(window.get_output_dir(), name))
+            shutil.move('tmp_files/tmp_txt.txt', '{}/{}.txt'.format(window.get_output_dir(), name))
+            msg = QMessageBox()
+            msg.setText("Save Complete")
+            msg.setWindowTitle("Save")
+            msg.exec_()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
+
     sys.exit(app.exec_())
